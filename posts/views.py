ceab54eba2a3da26fb5766dc62b8b8374de6d064
Zanin001 <bielzanin92@gmail.com>
2021-03-15 15:45:28 -0300

    Alterando PostDetalhes views para não herdar de UpdateViews

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
+from django.views.generic.base import View
 from comentarios.models import Comentario
 from typing import Annotated
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
@@ -56,28 +57,60 @@ class PostCategoria(PostIndex):
         return qs
 
 
-class PostDetalhes(UpdateView):
+# Class Post sem herdar do UpdateVews
+class PostDetalhes(View):
     template_name = 'posts/post_detalhes.html'
-    model=Post
-    form_class = FormComentario
-    context_object_name = 'post'
 
-    def get_context_data(self, **kwargs):
-        contexto = super().get_context_data(**kwargs)
-        post = self.get_object()
-        comentarios = Comentario.objects.filter(publicado_comentario=True, post_comentario=post.id)
-        contexto['comentarios'] = comentarios
-        return contexto
-    
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        pk = self.kwargs.get('pk')
+        post = get_object_or_404(Post, pk=pk, publicado_post=True)
+        self.contexto = {
+            'post': post,
+            'comentarios': Comentario.objects.filter(post_comentario=post, publicado_comentario = True),
+            'form': FormComentario(request.POST or None),
+        }
 
-    def form_valid(self, form):
-        post = self.get_object()
-        comentario = Comentario(**form.cleaned_data)
-        comentario.post_comentario = post
-        if self.request.user.is_authenticated:
-            comentario.usuario_comentario = self.request.user
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.contexto)
+
+    def post(self, request, *args, **kwargs):
+        form = self.contexto['form']
+        
+        if not form.is_valid():
+            return render(request, self.template_name, self.contexto)
+        comentario = form.save(commit=False)
+
+        if request.user.is_authenticated:
+            comentario.usuario_comentario = request.user
+        
+        comentario.post_comentario = self.conxtexto['post']
         comentario.save()
-        messages.success(self.request, 'Comentário enviado com sucesso')
-        return redirect('post_detalhes', pk=post.id)
+        messages.sucess(request, 'Seu comentário foi enviado')
+        return redirect('post_detalhes', pk=self.kwargs.get('pk'))
+
+#class PostDetalhes(UpdateView):
+#    template_name = 'posts/post_detalhes.html'
+#    model=Post
+#    form_class = FormComentario
+#    context_object_name = 'post'
+#
+#    def get_context_data(self, **kwargs):
+#        contexto = super().get_context_data(**kwargs)
+#        post = self.get_object()
+#        comentarios = Comentario.objects.filter(publicado_comentario=True, post_comentario=post.id)
+#        contexto['comentarios'] = comentarios
+#        return contexto
+
+
+#    def form_valid(self, form):
+#        post = self.get_object()
+#        comentario = Comentario(**form.cleaned_data)
+#        comentario.post_comentario = post
+#        if self.request.user.is_authenticated:
+#            comentario.usuario_comentario = self.request.user
+#        comentario.save()
+#        messages.success(self.request, 'Comentário enviado com sucesso')
+#        return redirect('post_detalhes', pk=post.id)
 
     
